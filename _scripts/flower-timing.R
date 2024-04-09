59935f2084457f674b1eb31dc60a71169772178d
Lucas Nell <lucnell@gmail.com>
2024-04-09 10:39:36 -0700

    phenology analysis focuses on individual plants, not averages

--- _scripts/flower-timing.R
+++ _scripts/flower-timing.R
@@ -6,8 +6,9 @@
 library(MASS)  # make sure this is loaded before tidyverse
 library(moments)
 library(tidyverse)
+library(sweetsoursong)
+library(ggtext)
 
-theme_set(theme_classic())
 
 
 phen_df <- paste0("~/Stanford_Drive/_sweetandsour/BIO47-data-2024-03-11/",
@@ -19,14 +20,6 @@ phen_df <- paste0("~/Stanford_Drive/_sweetandsour/BIO47-data-2024-03-11/",
            n_flowers = as.integer(round(n_flowers)),
            doy = yday(date))
 
-grp_phen_df <- phen_df |>
-    group_by(plant) |>
-    mutate(rel_flowers  = n_flowers / max(n_flowers)) |>
-    group_by(date, doy) |>
-    summarize(mean_flowers = mean(n_flowers),
-              total_flowers = sum(n_flowers),
-              rel_flowers = mean(rel_flowers),
-              .groups = "drop")
 
 phen_df |>
     ggplot(aes(date, n_flowers)) +
@@ -38,72 +31,105 @@ phen_df |>
           axis.text.x = element_text(color = "black"))
 
 
-grp_phen_df |>
-    ggplot(aes(date, mean_flowers)) +
-    geom_line() +
-    geom_point() +
-    ylab("Number of flowers") +
-    theme(axis.title.x = element_blank(),
-          axis.text.x = element_text(color = "black"))
-
-
-
 
 #' Using methods from "Skewness in bee and flower phenological distributions"
 #' by Stemkovski et al. (2022)
 #' (doi: 10.1002/ecy.3890)
 
-phen_vec <- rep(grp_phen_df$doy, round(grp_phen_df$mean_flowers))
-skewness(phen_vec)
-agostino.test(phen_vec)
-agostino.test(phen_vec, alternative = "greater")
 
-mods <- tibble(mod = c("cauchy", "chi-squared", "exponential", "gamma",
-                       "geometric", "lognormal", "logistic", "negative binomial",
-                       "normal", "Poisson", "t", "weibull"),
-               aic = NA_real_)
+phen_test_fits <- phen_df |>
+    split(~ plant) |>
+    map_dfr(\(x) {
+        phen_vec <- rep(x$doy, x$n_flowers)
+        skw <- skewness(phen_vec)
+        skw_tst <- agostino.test(phen_vec)
+        mods <- c("lognormal", "normal", "weibull") |>
+            set_names() |>
+            map_dbl(\(m) {
+                d <- tryCatch(fitdistr(phen_vec, m),
+                              error = \(e) NA,
+                              warning = \(w) NA)
+                if (inherits(d, "fitdistr")) {
+                    return(AIC(d))
+                } else return(NA)
+            })
+        bm <- names(mods)[which(mods == min(mods, na.rm = TRUE))]
+        tibble(plant = x$plant[[1]], skew = skw, skew_p = skw_tst$p.value,
+               best_mod = bm,
+               dAIC_norm = mods[["normal"]] - mods[[bm]])
+    }) |>
+    mutate(significant = factor(skew_p < 0.05, levels = c(FALSE, TRUE),
+                                labels = c("no", "yes")))
 
-for (m in mods$mod) {
-    d <- tryCatch(fitdistr(phen_vec, m), error = \(e) NA, warning = \(w) NA)
-    if (inherits(d, "fitdistr")) {
-        mods$aic[mods$mod == m] <- AIC(d)
-    }
-}
-
-mods |>
-    filter(!is.na(aic)) |>
-    mutate(d_aic = aic - min(aic)) |>
-    arrange(aic)
-
-
-weib_mod <- fitdistr(phen_vec, "weibull")
-norm_mod <- fitdistr(phen_vec, "normal")
-
-
-
-grp_phen_df |>
-    ggplot(aes(doy, mean_flowers)) +
-    geom_line(linewidth = 1) +
-    geom_point(size = 3) +
-    geom_line(data = tibble(doy = seq(min(grp_phen_df$doy), max(grp_phen_df$doy),
-                                      length.out = 101),
-                            mean_flowers = dweibull(doy, shape = weib_mod$estimate[["shape"]],
-                                         scale = weib_mod$estimate[["scale"]])) |>
-                  mutate(mean_flowers = mean_flowers / max(mean_flowers) * max(grp_phen_df$mean_flowers)),
-              color = "red", linewidth = 1) +
-    geom_line(data = tibble(doy = seq(min(grp_phen_df$doy), max(grp_phen_df$doy),
-                                      length.out = 101),
-                            mean_flowers = dnorm(doy, mean = norm_mod$estimate[["mean"]],
-                                         sd = norm_mod$estimate[["sd"]])) |>
-                  mutate(mean_flowers = mean_flowers / max(mean_flowers) *
-                             max(grp_phen_df$mean_flowers)),
-              color = "gray70", linewidth = 1) +
-    geom_text(data = tibble(doy = 100, lab = c("normal", "weibull"),
-                            mean_flowers = c(140, 130)),
-              aes(label = lab, color = lab), hjust = 0, fontface = "bold") +
-    ylab("Number of flowers") +
-    xlab("Day of year") +
-    scale_color_manual(values = c("gray70", "red"), guide = "none")
+#'
+#' Exactly half are skewed (15 / 30). Of these, the vast majority (13 / 15; 87%)
+#' are left skewed.
+#'
+#' Generally, Weibull distribution fits a left-skewed phenology better, while
+#' lognormal fits the right-skewed ones better.
+#'
+phen_test_fits |>
+    ggplot(aes(best_mod, skew)) +
+    geom_hline(yintercept = 0, linetype = "22", color = "gray70") +
+    geom_jitter(aes(color = significant), height = 0, width = 0.1, size = 3,
+                alpha = 0.5) +
+    geom_text(data = tibble(best_mod = factor("lognormal"),
+                            skew = c(-0.25, 0.25),
+                            lab = c("left skew", "right skew"),
+                            hj = c(1, 0)),
+              aes(label = lab, hjust = hj),
+              nudge_x = -0.5, angle = 90, vjust = 1, fontface = "italic") +
+    geom_text(data = phen_test_fits |>
+                  group_by(best_mod) |>
+                  summarize(skew = max(phen_test_fits$skew) * 1.1,
+                            n = sprintf("italic(n) == %i", n())),
+              aes(label = n), parse = TRUE) +
+    scale_color_manual("Significantly<br>skewed<br>(*P* < 0.05)",
+                       values = c("black", "red")) +
+    xlab("Best model") +
+    ylab("Skew") +
+    theme(legend.title = element_markdown())
 
 
 
+
+phen_fits <- phen_df |>
+    split(~ plant) |>
+    map_dfr(\(x) {
+        p <- x$plant[[1]]
+        m <- phen_test_fits$best_mod[phen_test_fits$plant == p]
+        phen_vec <- rep(x$doy, x$n_flowers)
+        mod <- fitdistr(phen_vec, m)
+        tibble(plant = p, model = m,
+               par = names(mod$estimate),
+               val = mod$estimate)
+    })
+
+
+phen_fits |>
+    split(~ model) |>
+    map(\(x) {
+        x |>
+            pivot_wider(names_from = par, values_from = val) |>
+            summarize(across(c(-plant, -model), list("mean" = mean, "sd" = sd)),
+                      n = n())
+    })
+
+
+# $lognormal
+# # A tibble: 1 × 5
+#   meanlog_mean meanlog_sd sdlog_mean sdlog_sd     n
+#          <dbl>      <dbl>      <dbl>    <dbl> <int>
+# 1         4.95      0.116     0.0756   0.0259     4
+#
+# $normal
+# # A tibble: 1 × 5
+#   mean_mean mean_sd sd_mean sd_sd     n
+#       <dbl>   <dbl>   <dbl> <dbl> <int>
+# 1      145.    7.72    11.9  3.20    14
+#
+# $weibull
+# # A tibble: 1 × 5
+#   shape_mean shape_sd scale_mean scale_sd     n
+#        <dbl>    <dbl>      <dbl>    <dbl> <int>
+# 1       15.2     3.54       151.     5.62    12
