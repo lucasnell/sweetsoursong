41bff70bfa2a1ac50f1874a37353f6ab398c71fa
Lucas Nell <lucnell@gmail.com>
2024-05-08 08:14:43 -0700

    Updates 2-patch constant F script

--- _scripts/two-patch-constantF.R
+++ _scripts/two-patch-constantF.R
@@ -6,7 +6,7 @@ library(parallel)
 
 options("mc.cores" = max(1L, detectCores()-2))
 
-outcome_pal <- c("coexist" = "#CB3694", # "#4477AA",
+outcome_pal <- c("coexist" = "#993399", # "#4477AA",
                  "coexist - within patch" = "#CC6666",  # "#66CCEE",
                  "yeast only" = "#FFCC33", # "#CCBB44",
                  "bacteria only" = "#333399") # "#AA3377")
@@ -24,9 +24,33 @@ high_low_run <- function(other_args) {
         other_args <- as.list(other_args)
     }
 
-    args <- list(dt = 0.1,
-                 max_t = 100,
-                 m = rep(0.1, 2))
+    args <- list(m = 0.1,
+                 # ----------*
+                 # within-plant dispersal rates:
+                 d_yp = 1.1,  # 1.5,  # pollinator-dependent for yeast (**)
+                 d_b0 = 0.3,  # pollinator-independent for bacteria (**)
+                 d_bp = 0.4,  # pollinator-dependent for bacteria (**)
+                 # -------------*
+                 # rates of immigration from non-focal-plant sources:
+                 g_yp = 0, # 0.005, # pollinator-dependent for yeast (**)
+                 g_b0 = 0, # 0.02, # pollinator-independent for bacteria (**)
+                 g_bp = 0, # 0.002,  # pollinator-dependent for bacteria
+                 # -------------*
+                 # pollinators:
+                 L_0 = 0.5,  # half saturation ratio for P -> dispersal (--)
+                 u = 0,      # power for B -> P (??)
+                 X = 0,      # attraction for non-focal plants (??)
+                 #
+                 # (**)  = value from Song et al. (submitted)
+                 # (--)  = value different from Song et al. (submitted)
+                 # (??) = value should be varied bc I have no clue what to use
+                 #
+                 dt = 0.1,
+                 max_t = 500)
+
+    for (n in names(args)[!names(args) %in% c("max_t","dt","X","u")]) {
+        args[[n]] <- rep(args[[n]], 2)
+    }
 
     stopifnot(inherits(other_args, "list"))
     stopifnot(! is.null(names(other_args)) && ! any(names(other_args) == ""))
@@ -36,22 +60,22 @@ high_low_run <- function(other_args) {
         other_args[["max_t"]] <- NULL
     }
 
-    stopifnot(! any(names(other_args) %in% names(args)))
-
     for (n in names(other_args)) {
-        if (n %in% c("P_total", "S_0", "X")) next
-        if (length(other_args[[n]]) == 1) other_args[[n]] <- rep(other_args[[n]], 2)
+        if (! n %in% c("P_total", "u", "X") && length(other_args[[n]]) == 1) {
+            other_args[[n]] <- rep(other_args[[n]], 2)
+        }
+        args[[n]] <- other_args[[n]]
     }
 
     # Below, I made the low-density one a bit different to avoid weirdness
     # with exactly equal starting values
-    all_runs_args <-list("high B" = list(Y0 = c(0.11, 0.1),  B0 = c(0.5, 0.5)),
-                         "high Y" = list(Y0 = c(0.5, 0.5), B0 = c(0.1, 0.11)),
+    all_runs_args <-list("high B" = list(Y0 = c(0.2, 0.1),  B0 = c(0.5, 0.5)),
+                         "high Y" = list(Y0 = c(0.5, 0.5), B0 = c(0.1, 0.2)),
                          "one of each" = list(Y0 = c(0.5, 0.1), B0 = c(0.1, 0.5)))
 
     all_runs <- all_runs_args |>
         imap_dfr(\(x, n) {
-            c(args, other_args, x) |>
+            c(args, x) |>
                 do.call(what = landscape_constantF_ode) |>
                 as_tibble() |>
                 mutate(run = n)
@@ -65,28 +89,13 @@ high_low_run <- function(other_args) {
 }
 
 
-tibble(# ----------
-       # within-plant dispersal rates:
-       d_yp = 1.0, # 1.5,  # pollinator-dependent for yeast (**)
-       d_b0 = 0.3,  # pollinator-independent for bacteria (**)
-       d_bp = 0.4,  # pollinator-dependent for bacteria (**)
-       # -------------
-       # rates of immigration from non-focal-plant sources:
-       g_yp = 0, # 0.005, # pollinator-dependent for yeast (**)
-       g_b0 = 0, # 0.02, # pollinator-independent for bacteria (**)
-       g_bp = 0, # 0.002,  # pollinator-dependent for bacteria
-       # -------------
-       # pollinators:
-       L_0 = 0.5,          # half saturation ratio for P -> dispersal (**)
-       S_0 = 10e3,          # half saturation ratio for B -> P (??)
-       X = 0,              # attraction for nearby flowers other than focal plant (??)
-       #
-       # (**)  = value from Song et al. (submitted)
-       # (??) = value should be varied bc I have no clue what to use
-       #
-       max_t = 800
-) |>
-    high_low_run() |>
+noBP_runs <- tibble(u = 0) |>
+    high_low_run()
+withBP_runs <- tibble(u = 2) |>
+    high_low_run()
+
+
+noBP_plots <- noBP_runs |>
     (\(x) {
         print(group_by(x, run) |>
                   filter(t == max(t)) |>
@@ -97,21 +106,70 @@ tibble(# ----------
         return(x)
     })() |>
     filter(t %% 1 == 0) |>
+    split(~ run) |>
+    map(\(x) {
+        x |>
+            pivot_longer(Y:P, names_to = "type", values_to = "density") |>
+            mutate(type = factor(type, levels = c("Y", "B", "P"),
+                                 labels = c("yeast", "bacteria", "pollinators"))) |>
+            ggplot(aes(t, density)) +
+            geom_hline(yintercept = 0, linewidth = 1, color = "gray80") +
+            geom_line(aes(color = type, linetype = type), linewidth = 1) +
+            facet_grid( ~ p) +
+            xlab("Time (days)") +
+            ylim(0, 0.95) +
+            scale_color_manual(NULL, values = spp_pal) +
+            scale_linetype_manual(NULL, values = c("solid", "solid", "24")) +
+            theme(strip.text = element_blank(), strip.background = element_blank(),
+                  legend.position = "none", axis.title = element_blank()) +
+            NULL
+    })
+
+withBP_plots <- withBP_runs |>
+    (\(x) {
+        print(group_by(x, run) |>
+                  filter(t == max(t)) |>
+                  summarize(Y = mean(Y),
+                            B = mean(B),
+                            N = 1 - Y - B,
+                            .groups = "drop"))
+        return(x)
+    })() |>
+    filter(t %% 1 == 0) |>
+    split(~ run) |>
+    map(\(x) {
+        x |>
+            pivot_longer(Y:P, names_to = "type", values_to = "density") |>
+            mutate(type = factor(type, levels = c("Y", "B", "P"),
+                                 labels = c("yeast", "bacteria", "pollinators"))) |>
+            ggplot(aes(t, density)) +
+            geom_hline(yintercept = 0, linewidth = 1, color = "gray80") +
+            geom_line(aes(color = type, linetype = type), linewidth = 1) +
+            facet_grid( ~ p) +
+            xlab("Time (days)") +
+            ylim(0, 0.95) +
+            scale_color_manual(NULL, values = spp_pal) +
+            scale_linetype_manual(NULL, values = c("solid", "solid", "24")) +
+            theme(strip.text = element_blank(), strip.background = element_blank(),
+                  legend.position = "none", axis.title = element_blank()) +
+            NULL
+    })
+
+
+noBP_plots[["one of each"]]
+withBP_plots[["one of each"]]
+
+# # For talk:
+# ggsave("_figures/2patch-constF-noBP.png", noBP_plots[["high B"]],
+#        width = 5, height = 3)
+# ggsave("_figures/2patch-constF-withBP.png", withBP_plots[["high B"]],
+#        width = 5, height = 3)
+
+
+noBP_runs |>
     filter(run == "high B") |>
-    pivot_longer(Y:P, names_to = "type", values_to = "density") |>
-    mutate(type = factor(type, levels = c("Y", "B", "P"),
-                         labels = c("yeast", "bacteria", "pollinators"))) |>
-    ggplot(aes(t, density)) +
-    geom_hline(yintercept = 0, linewidth = 1, color = "gray80") +
-    geom_line(aes(color = type, linetype = type), linewidth = 1) +
-    # facet_grid(run ~ p) +
-    facet_grid( ~ p) +
-    xlab("Time (days)") +
-    scale_color_manual(NULL, values = spp_pal) +
-    scale_linetype_manual(NULL, values = c("solid", "solid", "24")) +
-    theme(strip.text = element_blank(), strip.background = element_blank(),
-          legend.position = "none", axis.title = element_blank()) +
-    NULL
+    filter(t == max(t))
+
 
 
 
@@ -125,12 +183,12 @@ equil_run <- function(other_args, .n_plants = 2L, .precision = 1e-4) {
     args <- list(dt = 0.1,
                  max_t = 1000,
                  m = rep(0.1, .n_plants),
-                 # -------------
+                 # -------------*
                  # rates of immigration from non-focal-plant sources:
                  g_yp = rep(0,.n_plants), # 0.005, # pollinator-dependent for yeast (**)
                  g_b0 = rep(0,.n_plants), # 0.02, # pollinator-independent for bacteria (**)
                  g_bp = rep(0,.n_plants), # 0.002,  # pollinator-dependent for bacteria
-                 # -------------
+                 # -------------*
                  # pollinators:
                  X = 0               # attraction for nearby flowers other than focal plant (??)
     )
@@ -146,7 +204,7 @@ equil_run <- function(other_args, .n_plants = 2L, .precision = 1e-4) {
     stopifnot(! any(names(other_args) %in% names(args)))
 
     for (n in names(other_args)) {
-        if (n %in% c("P_total", "S_0", "X")) next
+        if (n %in% c("P_total", "u", "X")) next
         if (length(other_args[[n]]) == 1) other_args[[n]] <- rep(other_args[[n]], .n_plants)
     }
 
@@ -194,15 +252,15 @@ equil_run <- function(other_args, .n_plants = 2L, .precision = 1e-4) {
 if (! file.exists("_data/two-patch-constF-equil.rds")) {
 
     #' 2700 with 6 threads takes ~2 min
-    equil_df <- crossing(# ----------
+    equil_df <- crossing(# ----------*
                          # within-plant dispersal rates:
                          d_yp = 1.1 + -4:4/10, # 1.5,  # pollinator-dependent for yeast (**)
                          d_b0 = 0.3 + -2:2/10,  # pollinator-independent for bacteria (**)
                          d_bp = 0.4 + -3:1/10,  # pollinator-dependent for bacteria (**)
-                         # -------------
+                         # -------------*
                          # pollinators:
-                         L_0 = c(0.1, 0.5, 0.9),          # for P -> dispersal (**)
-                         S_0 = c(0.6, 10e3),              # half saturation for B -> P (??)
+                         L_0 = c(0.1, 0.5, 0.9), # for P -> dispersal (**)
+                         u = c(0, 1, 2),         # power for B -> P (??)
                          max_t = 10e3L) |>
         (\(x) split(x, 1:nrow(x)))() |>
         map(as.list) |>
@@ -221,8 +279,8 @@ if (! file.exists("_data/two-patch-constF-equil.rds")) {
 outcome_plot_prep <- function(x) {
     x |>
         mutate(Yprop = (Y1 + Y2) / (Y1 + Y2 + B1 + B2),
-               S_0 = factor(S_0, levels = rev(sort(unique(S_0))),
-                          labels = sprintf("S_0 = %.1f", rev(sort(unique(S_0))))),
+               u = factor(u, levels = sort(unique(u)),
+                          labels = sprintf("u = %.0f", sort(unique(u)))),
                outcome = case_when(Yprop < 1e-3 ~ "bacteria only",
                                    Yprop > (1-1e-3) ~ "yeast only",
                                    Y1 >= 1e-3 & Y1 <= (1-1e-3) &
@@ -240,7 +298,7 @@ equil_df |>
     scale_fill_manual(values = outcome_pal) +
     xlab("Yeast pollinator-dependent dispersal") +
     ylab("Bacteria pollinator-independent dispersal") +
-    facet_grid(run ~ S_0)
+    facet_grid(run ~ u)
 
 
 equil_df |>
@@ -251,24 +309,25 @@ equil_df |>
     scale_fill_manual(values = outcome_pal) +
     xlab("Yeast pollinator-dependent dispersal") +
     ylab("Bacteria pollinator-dependent dispersal") +
-    facet_grid(run ~ S_0)
+    facet_grid(run ~ u)
 
 
 
-# For Yosemite talk
-equil_df |>
+# For talk:
+equil_p <- equil_df |>
     filter(L_0 == 0.5, d_bp == 0.4) |>
-    filter(run == "one of each") |>
+    filter(run == "one of each", u %in% range(u)) |>
     outcome_plot_prep() |>
     ggplot(aes(d_yp, d_b0, fill = outcome)) +
     geom_raster() +
     scale_fill_manual(values = outcome_pal) +
     xlab("Yeast pollinator-dependent dispersal") +
     ylab("Bacteria pollinator-independent dispersal") +
-    facet_wrap(~ S_0, nrow = 1) +
+    facet_wrap(~ u, nrow = 1) +
     theme(legend.position = "none", axis.title = element_blank(),
           strip.text = element_blank(), strip.background = element_blank())
 
+# ggsave("_figures/2patch-constF-equil.png", width = 5, height = 2.5)
 
 
 
