8ff319a80e186f191a488ed7435773d7e8188f74
Lucas Nell <lucnell@gmail.com>
2024-08-01 13:45:12 -0700

    Added back "open" simulations with fixed grouping for diversity calc

--- _scripts/landscape-constantF-stoch.R
+++ _scripts/landscape-constantF-stoch.R
@@ -9,6 +9,7 @@ setThreadOptions(numThreads = max(defaultNumThreads() - 2L, 1L))
 if (file.exists(".Rprofile")) source(".Rprofile")
 
 stoch_sims_file <- "_data/constantF-stoch-sims.rds"
+open_stoch_sims_file <- "_data/constantF-stoch-open-sims.rds"
 
 
 outcome_pal <- c("coexist" = "#993399", # "#4477AA",
@@ -189,8 +190,8 @@ if (! file.exists(stoch_sims_file)) {
 
 
 
-season_sigma__ <- 10
-n_plants__ <- 2
+season_sigma__ <- 0
+n_plants__ <- 10
 
 map(names(d_yp__), \(n) {
     p <- stoch_sim_df |>
@@ -225,6 +226,149 @@ map(names(d_yp__), \(n) {
     c(list(guides = "collect")) |>
     do.call(what = wrap_plots)
 
+
+
+
+
+# =============================================================================*
+# =============================================================================*
+
+# Open system sims ----
+
+# =============================================================================*
+# =============================================================================*
+
+
+
+# Summarize for an open system where extinctions are temporary.
+# In this case, output Bray-Curtis dissimilarity and Shannon diversity index
+summ_open_run <- function(even_run_output) {
+
+    .np <- length(unique(even_run_output$p))
+
+    even_run_output |>
+        group_by(rep) |>
+        summarize(dive = diversity_vector(Y, B, group_size = .np),
+                  diss = dissimilarity_vector(Y, B, group_size = .np, TRUE))
+
+}
+
+
+
+
+
+if (! file.exists(open_stoch_sims_file)) {
+
+    # Takes ~70 min
+    open_stoch_sim_df <- crossing(.u = 0:10,
+                                  .d_yp = d_yp__,
+                                  .n_sigma = c(100, 200, 400),
+                                  .season_surv = c(0.05, 0.1, 0.2),
+                                  .season_sigma = c(0, 10),
+                                  .np = c(2, 10)) |>
+        # Don't do this in parallel bc landscape_constantF_stoch_ode is already
+        # doing that
+        pmap_dfr(\(.u, .d_yp, .n_sigma, .season_surv, .season_sigma, .np) {
+            list(u = .u, d_yp = .d_yp, n_sigma = .n_sigma,
+                 season_surv = .season_surv, season_sigma = .season_sigma,
+                 g_yp = 0.005, g_b0 = 0.02, g_bp = 0.002) |>
+                even_run(np = .np) |>
+                summ_open_run() |>
+                mutate(u = .u, d_yp = .d_yp, n_sigma = .n_sigma,
+                       season_surv = .season_surv,
+                       season_sigma = .season_sigma,
+                       n_plants = .np)
+        })
+    write_rds(open_stoch_sim_df, open_stoch_sims_file)
+
+} else {
+
+    open_stoch_sim_df <- read_rds(open_stoch_sims_file)
+
+}
+
+
+
+season_sigma__ <- 0
+n_plants__ <- 2L
+
+
+
+
+map(names(d_yp__), \(n) {
+    dd <- open_stoch_sim_df |>
+        filter(d_yp == d_yp__[[n]],
+               season_sigma == season_sigma__,
+               n_plants == n_plants__) |>
+        mutate(n_sigma = paste("n[sigma] ==", n_sigma),
+               season_surv = paste("s ==", season_surv)) |>
+        pivot_longer(dive:diss) |>
+        mutate(name = factor(name, levels = c("diss", "dive"),
+                             labels = c("dissimilarity", "diversity")))
+    dds <- dd |>
+        group_by(u, d_yp, n_sigma, season_surv, season_sigma, name) |>
+        summarize(value = median(value), .groups = "drop")
+    p <- dd |>
+        ggplot(aes(u, value, color = name)) +
+        ggtitle(paste("Outcome without stochasticity:\n", n)) +
+        geom_point(shape = 1, alpha = 0.1) +
+        geom_line(data = dds, linewidth = 1) +
+        facet_grid(season_surv ~ n_sigma, labeller = label_parsed) +
+        scale_y_continuous("Measure value") +
+        scale_x_continuous("u", breaks = seq(0, 10, 2.5),
+                           labels = c("0", "", "5", "", "10")) +
+        scale_color_viridis_d(NULL, option = "magma", begin = 0.3, end = 0.8) +
+        theme(plot.title = element_text(size = 12,
+                                        margin = margin(0,0,0,b=6)))
+    if (n != names(d_yp__)[[1]]) {
+        p <- p + theme(axis.title.y = element_blank(),
+                       axis.text.y = element_blank())
+    }
+    if (n != tail(names(d_yp__), 1)) {
+        p <- p + theme(strip.text.y = element_blank())
+    }
+    return(p)
+}) |>
+    c(list(guides = "collect")) |>
+    do.call(what = wrap_plots)
+
+
+
+
+
+
+# u__ <- 0
+#
+# z <- list(u = u__, d_yp = d_yp__[[2]], n_sigma = 200,
+#           n_reps = 1,
+#      g_yp = 0.005,
+#      g_b0 = 0.02,
+#      g_bp = 0.002,
+#      season_surv = 0.1, season_sigma = 0) |>
+#     even_run(np = 2L)
+# zz <- z |>
+#     group_by(t) |>
+#     summarize(Y = sum(Y), B = sum(B))
+#
+#
+# z |>
+#     select(-P) |>
+#     pivot_longer(Y:B, names_to = "species", values_to = "density") |>
+#     mutate(species = factor(species, levels = c("Y", "B"),
+#                             labels = c("yeast", "bacteria")),
+#            p = factor(as.integer(p))) |>
+#     ggplot(aes(t, density, color = species)) +
+#     geom_line() +
+#     facet_wrap(~ p) +
+#     scale_color_manual(values = spp_pal)
+#
+# zz |>
+#     pivot_longer(Y:B, names_to = "species", values_to = "density") |>
+#     mutate(species = factor(species, levels = c("Y", "B"),
+#                             labels = c("yeast", "bacteria"))) |>
+#     ggplot(aes(t, density, color = species)) +
+#     geom_line() +
+#     scale_color_manual(values = spp_pal)
 
 
 
