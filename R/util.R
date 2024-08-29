ab98053c6014a28978b2d99ca56ced588e9f97e4
Lucas Nell <lucnell@gmail.com>
2024-08-29 10:46:34 -0700

    Allowed function input to save_plot to have arguments

--- R/util.R
+++ R/util.R
@@ -5,12 +5,14 @@
 #' @param w Width in inches
 #' @param h Height in inches
 #' @param seed Integer to seed RNG for consistent jittering.
+#' @param fun_args List containing arguments to `p` if it's a function and has
+#'     required arguments.
 #' @param ... Other arguments to `cairo_pdf`
 #'
 #' @return `NULL`
 #' @export
 #'
-save_plot <- function(fn, p, w, h, seed = NULL, ...) {
+save_plot <- function(fn, p, w, h, seed = NULL, fun_args = NULL, ...) {
     ext <- tail(strsplit(fn, "\\.")[[1]], 1)
     if (ext != "pdf") stop("ERROR: file name extension must be \".pdf\"")
     fn_dir <- dirname(fn)
@@ -18,7 +20,11 @@ save_plot <- function(fn, p, w, h, seed = NULL, ...) {
     if (!is.null(seed)) set.seed(seed)
     cairo_pdf(filename = fn, width = w, height = h, ...)
     if (is.function(p)) {
-        p()
+        if (is.null(fun_args)) {
+            p()
+        } else {
+            do.call(p, fun_args)
+        }
     } else {
         plot(p)
     }
