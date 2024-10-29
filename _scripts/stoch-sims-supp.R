fa578d12cb77a83d5978e716fd44591f5703928a
Lucas Nell <lucnell@gmail.com>
2024-10-29 16:29:09 -0400

    stoch-sims uses updated pkg

--- _scripts/stoch-sims-supp.R
+++ _scripts/stoch-sims-supp.R
@@ -63,18 +63,17 @@ d_yp__ = c("bacteria only" = 0.886308 - 0.1,
 
 if (! file.exists(big_stoch_sims_file)) {
 
-    # Takes ~26 min with 6 threads
-    t0 <- Sys.time()
+    # Takes ~34 min with 6 threads
     set.seed(1472844374)
     stoch_sim_df <- crossing(.u = 0:10,
                              .d_yp = d_yp__,
                              .season_surv = c(0.015, 0.03, 0.06),
-                             .sigma_s = c(0, 0.5, 1, 10)) |>
+                             .q = c(0, 0.25, 0.5, 0.75, 1)) |>
         # Don't do this in parallel bc plant_metacomm_stoch is already
         # doing that
-        pmap_dfr(\(.u, .d_yp, .season_surv, .sigma_s) {
+        pmap_dfr(\(.u, .d_yp, .season_surv, .q) {
             plant_metacomm_stoch(np = 100L, u = .u, d_yp = .d_yp,
-                                 sigma_s = .sigma_s,
+                                 q = .q,
                                  no_immig = TRUE,
                                  n_sigma = 100,
                                  season_surv = .season_surv,
@@ -83,11 +82,9 @@ if (! file.exists(big_stoch_sims_file)) {
                                  # summarize by rep:
                                  summarize = "rep") |>
                 mutate(u = .u, d_yp = .d_yp, season_surv = .season_surv,
-                       sigma_s = .sigma_s) |>
-                select(u, d_yp, season_surv, sigma_s, everything())
+                       q = .q) |>
+                select(u, d_yp, season_surv, q, everything())
         }, .progress = TRUE)
-    t1 <- Sys.time()
-    t1 - t0; rm(t0, t1)
 
     write_rds(stoch_sim_df, big_stoch_sims_file)
 
@@ -117,16 +114,24 @@ add_factors <- function(d) {
         d <- mutate(d, d_yp = factor(d_yp, levels = unname(d_yp__),
                                      labels = names(d_yp__)))
     }
-    if ("sigma_s" %in% colnames(d) && !is.factor(d[["sigma_s"]])) {
-        d <- mutate(d, sigma_s = factor(sigma_s))
+    if ("q" %in% colnames(d) && !is.factor(d[["q"]])) {
+        d <- mutate(d, q = factor(q))
     }
     if ("species" %in% colnames(d) && !is.factor(d[["species"]])) {
-        if (all(d$species == "Y" | d$species == "B")) {
+        if (all(d$species %in% c("Y", "B"))) {
             d <- mutate(d, species = factor(species, levels = c("Y", "B"),
                                             labels = c("yeast", "bacteria")))
-        } else if (all(d$species == "yeast" | d$species == "bacteria")) {
+        } else if (all(d$species %in% c("yeast", "bacteria"))) {
             d <- mutate(d, species = factor(species,
                                             levels = c("yeast", "bacteria")))
+        } else if (all(d$species %in% c("Y", "B", "P"))) {
+            d <- mutate(d, species = factor(species, levels = c("Y", "B", "P"),
+                                            labels = c("yeast", "bacteria",
+                                                       "pollinators")))
+        } else if (all(d$species %in% c("yeast", "bacteria", "pollinators"))) {
+            d <- mutate(d, species = factor(species,
+                                            levels = c("yeast", "bacteria",
+                                                       "pollinators")))
         } else {
             stop("strange values in d$species")
         }
@@ -174,26 +179,26 @@ add_outcome_props <- function(d, .threshold = 1e-6){
 # =====================================================*
 
 
-outcome_plotter <- function(season_surv__, sigma_s__ = NULL) {
+outcome_plotter <- function(season_surv__, q__ = NULL) {
 
-    if (is.null(sigma_s__)) {
+    if (is.null(q__)) {
         p <- stoch_sim_df |>
             filter(season_surv == season_surv__) |>
-            select(sigma_s, u, d_yp, minY, minB, maxY, maxB, meanY, meanB) |>
+            select(q, u, d_yp, minY, minB, maxY, maxB, meanY, meanB) |>
             add_outcome_props() |>
             add_factors() |>
             ggplot(aes(u, prop, color = outcome)) +
             ggtitle(sprintf("s = %s", season_surv__)) +
-            facet_grid(sigma_s ~ d_yp)
+            facet_grid(q ~ d_yp)
     } else {
         p <- stoch_sim_df |>
             add_factors() |>
             filter(season_surv == season_surv__,
-                   sigma_s == sigma_s__) |>
+                   q == q__) |>
             select(u, d_yp, minY, minB, maxY, maxB, meanY, meanB) |>
             add_outcome_props() |>
             ggplot(aes(u, prop, color = outcome)) +
-            ggtitle(sprintf("s = %s, sigma_s = %s", season_surv__, sigma_s__)) +
+            ggtitle(sprintf("s = %s, q = %s", season_surv__, q__)) +
             facet_wrap( ~ d_yp, nrow = 1)
     }
     p <- p +
@@ -218,7 +223,7 @@ outcome_plotter <- function(season_surv__, sigma_s__ = NULL) {
 
 
 abundance_plotter <- function(season_surv__,
-                              sigma_s__ = NULL,
+                              q__ = NULL,
                               free_y = FALSE) {
 
     .scales <- "fixed"
@@ -233,25 +238,25 @@ abundance_plotter <- function(season_surv__,
         .ylabels <- waiver()
     }
 
-    if (is.null(sigma_s__)) {
+    if (is.null(q__)) {
         dd <- stoch_sim_df |>
             filter(season_surv == season_surv__) |>
-            select(sigma_s, u, d_yp, meanY, meanB) |>
+            select(q, u, d_yp, meanY, meanB) |>
             pivot_longer(meanY:meanB, names_to = "species") |>
             mutate(species = str_sub(species, 5L, 5L)) |>
             add_factors()
         dds <- dd |>
-            group_by(sigma_s, u, d_yp, species) |>
+            group_by(q, u, d_yp, species) |>
             summarize(value = mean(value), .groups = "drop")
         p <- dd |>
             ggplot(aes(u, value, color = species)) +
             ggtitle(sprintf("s = %s", season_surv__)) +
-            facet_grid(sigma_s ~ d_yp, scales = .scales)
+            facet_grid(q ~ d_yp, scales = .scales)
     } else {
         dd <- stoch_sim_df |>
             add_factors() |>
             filter(season_surv == season_surv__,
-                   sigma_s == sigma_s__) |>
+                   q == q__) |>
             select(u, d_yp, meanY, meanB) |>
             pivot_longer(meanY:meanB, names_to = "species") |>
             mutate(species = str_sub(species, 5L, 5L)) |>
@@ -261,7 +266,7 @@ abundance_plotter <- function(season_surv__,
             summarize(value = mean(value), .groups = "drop")
         p <- dd |>
             ggplot(aes(u, value, color = species)) +
-            ggtitle(sprintf("s = %s, sigma_s = %s", season_surv__, sigma_s__)) +
+            ggtitle(sprintf("s = %s, q = %s", season_surv__, q__)) +
             facet_grid( ~ d_yp, scales = .scales)
     }
 
@@ -311,6 +316,35 @@ outcome_plotter(median(unique(stoch_sim_df$season_surv)), 0.0) /
 
 
 
+z <- plant_metacomm_stoch(np = 2L, u = 5, d_yp = 0.9,
+                          q = 1,
+                          no_immig = TRUE,
+                          n_sigma = 100,
+                          season_surv = 0.05,
+                          max_t = 750,
+                          burnin = 0,
+                          summarize = "none")
+
+tibble(t0 = c(z$Y[z$t == 150], z$B[z$t == 150]),
+       t1 = c(z$Y[z$t == 150.1], z$B[z$t == 150.1]),
+       species = rep(c("Y", "B"), each = sum(z$t == 150))) |>
+    add_factors() |>
+    ggplot(aes(t0, t1, color = species)) +
+    geom_point() +
+    scale_color_manual(values = spp_pal)
+
+
+z |>
+    filter(rep %in% 1:3) |>
+    # filter(t %% 1 == 0) |>
+    select(-P) |>
+    pivot_longer(Y:B, names_to = "species", values_to = "density") |>
+    mutate(id = interaction(species, p, rep)) |>
+    add_factors() |>
+    ggplot(aes(t, density, color = species)) +
+    geom_line(aes(group = id), linewidth = 0.5, alpha = 0.5) +
+    facet_grid(p ~ rep) +
+    scale_color_manual(values = spp_pal)
 
 
 
