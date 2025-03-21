522d17f17a75be06624db7190aecce16c8265657
Lucas Nell <lucnell@gmail.com>
2025-03-21 12:32:06 -0400

    Removed unused code from package

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,37 +11,6 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// one_flower_ode
-NumericMatrix one_flower_ode(const double& dt, const double& max_t, const double& Y_delay, const double& B_delay, const double& Y0, const double& B0, const double& A0, const double& H0, const double& D, double A_0, const double& r_Y, const double& r_B, const double& m_Y, const double& m_B, const double& e_B, const double& q_Y, const double& q_B, const double& c_Y, const double& c_B, const double& h_B, const double& h_Y);
-RcppExport SEXP _sweetsoursong_one_flower_ode(SEXP dtSEXP, SEXP max_tSEXP, SEXP Y_delaySEXP, SEXP B_delaySEXP, SEXP Y0SEXP, SEXP B0SEXP, SEXP A0SEXP, SEXP H0SEXP, SEXP DSEXP, SEXP A_0SEXP, SEXP r_YSEXP, SEXP r_BSEXP, SEXP m_YSEXP, SEXP m_BSEXP, SEXP e_BSEXP, SEXP q_YSEXP, SEXP q_BSEXP, SEXP c_YSEXP, SEXP c_BSEXP, SEXP h_BSEXP, SEXP h_YSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const double& >::type dt(dtSEXP);
-    Rcpp::traits::input_parameter< const double& >::type max_t(max_tSEXP);
-    Rcpp::traits::input_parameter< const double& >::type Y_delay(Y_delaySEXP);
-    Rcpp::traits::input_parameter< const double& >::type B_delay(B_delaySEXP);
-    Rcpp::traits::input_parameter< const double& >::type Y0(Y0SEXP);
-    Rcpp::traits::input_parameter< const double& >::type B0(B0SEXP);
-    Rcpp::traits::input_parameter< const double& >::type A0(A0SEXP);
-    Rcpp::traits::input_parameter< const double& >::type H0(H0SEXP);
-    Rcpp::traits::input_parameter< const double& >::type D(DSEXP);
-    Rcpp::traits::input_parameter< double >::type A_0(A_0SEXP);
-    Rcpp::traits::input_parameter< const double& >::type r_Y(r_YSEXP);
-    Rcpp::traits::input_parameter< const double& >::type r_B(r_BSEXP);
-    Rcpp::traits::input_parameter< const double& >::type m_Y(m_YSEXP);
-    Rcpp::traits::input_parameter< const double& >::type m_B(m_BSEXP);
-    Rcpp::traits::input_parameter< const double& >::type e_B(e_BSEXP);
-    Rcpp::traits::input_parameter< const double& >::type q_Y(q_YSEXP);
-    Rcpp::traits::input_parameter< const double& >::type q_B(q_BSEXP);
-    Rcpp::traits::input_parameter< const double& >::type c_Y(c_YSEXP);
-    Rcpp::traits::input_parameter< const double& >::type c_B(c_BSEXP);
-    Rcpp::traits::input_parameter< const double& >::type h_B(h_BSEXP);
-    Rcpp::traits::input_parameter< const double& >::type h_Y(h_YSEXP);
-    rcpp_result_gen = Rcpp::wrap(one_flower_ode(dt, max_t, Y_delay, B_delay, Y0, B0, A0, H0, D, A_0, r_Y, r_B, m_Y, m_B, e_B, q_Y, q_B, c_Y, c_B, h_B, h_Y));
-    return rcpp_result_gen;
-END_RCPP
-}
 // plant_metacomm_stoch_cpp
 arma::mat plant_metacomm_stoch_cpp(const size_t& n_reps, const std::vector<double>& m, const std::vector<double>& d_yp, const std::vector<double>& d_b0, const std::vector<double>& d_bp, const std::vector<double>& g_yp, const std::vector<double>& g_b0, const std::vector<double>& g_bp, const std::vector<double>& L_0, const double& u, const double& X, const std::vector<double>& Y0, const std::vector<double>& B0, const double& n_sigma, const size_t& season_len, const double& season_surv, const double& q, const bool& open_sys, const double& dt, const double& max_t, const double& burnin, const size_t& save_every, const bool& begin_end, const int& summarize);
 RcppExport SEXP _sweetsoursong_plant_metacomm_stoch_cpp(SEXP n_repsSEXP, SEXP mSEXP, SEXP d_ypSEXP, SEXP d_b0SEXP, SEXP d_bpSEXP, SEXP g_ypSEXP, SEXP g_b0SEXP, SEXP g_bpSEXP, SEXP L_0SEXP, SEXP uSEXP, SEXP XSEXP, SEXP Y0SEXP, SEXP B0SEXP, SEXP n_sigmaSEXP, SEXP season_lenSEXP, SEXP season_survSEXP, SEXP qSEXP, SEXP open_sysSEXP, SEXP dtSEXP, SEXP max_tSEXP, SEXP burninSEXP, SEXP save_everySEXP, SEXP begin_endSEXP, SEXP summarizeSEXP) {
@@ -101,92 +70,10 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// logit
-NumericVector logit(NumericVector p);
-RcppExport SEXP _sweetsoursong_logit(SEXP pSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type p(pSEXP);
-    rcpp_result_gen = Rcpp::wrap(logit(p));
-    return rcpp_result_gen;
-END_RCPP
-}
-// inv_logit
-NumericVector inv_logit(NumericVector x);
-RcppExport SEXP _sweetsoursong_inv_logit(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(inv_logit(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// dissimilarity
-double dissimilarity(NumericVector yeast, NumericVector bact);
-RcppExport SEXP _sweetsoursong_dissimilarity(SEXP yeastSEXP, SEXP bactSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type yeast(yeastSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type bact(bactSEXP);
-    rcpp_result_gen = Rcpp::wrap(dissimilarity(yeast, bact));
-    return rcpp_result_gen;
-END_RCPP
-}
-// dissimilarity_vector
-NumericVector dissimilarity_vector(NumericVector yeast, NumericVector bact, const size_t& group_size, const bool& overall_mean);
-RcppExport SEXP _sweetsoursong_dissimilarity_vector(SEXP yeastSEXP, SEXP bactSEXP, SEXP group_sizeSEXP, SEXP overall_meanSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type yeast(yeastSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type bact(bactSEXP);
-    Rcpp::traits::input_parameter< const size_t& >::type group_size(group_sizeSEXP);
-    Rcpp::traits::input_parameter< const bool& >::type overall_mean(overall_meanSEXP);
-    rcpp_result_gen = Rcpp::wrap(dissimilarity_vector(yeast, bact, group_size, overall_mean));
-    return rcpp_result_gen;
-END_RCPP
-}
-// diversity
-double diversity(NumericVector yeast, NumericVector bact, double zero_threshold);
-RcppExport SEXP _sweetsoursong_diversity(SEXP yeastSEXP, SEXP bactSEXP, SEXP zero_thresholdSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type yeast(yeastSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type bact(bactSEXP);
-    Rcpp::traits::input_parameter< double >::type zero_threshold(zero_thresholdSEXP);
-    rcpp_result_gen = Rcpp::wrap(diversity(yeast, bact, zero_threshold));
-    return rcpp_result_gen;
-END_RCPP
-}
-// diversity_vector
-double diversity_vector(NumericVector yeast, NumericVector bact, const size_t& group_size, double zero_threshold);
-RcppExport SEXP _sweetsoursong_diversity_vector(SEXP yeastSEXP, SEXP bactSEXP, SEXP group_sizeSEXP, SEXP zero_thresholdSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type yeast(yeastSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type bact(bactSEXP);
-    Rcpp::traits::input_parameter< const size_t& >::type group_size(group_sizeSEXP);
-    Rcpp::traits::input_parameter< double >::type zero_threshold(zero_thresholdSEXP);
-    rcpp_result_gen = Rcpp::wrap(diversity_vector(yeast, bact, group_size, zero_threshold));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_sweetsoursong_one_flower_ode", (DL_FUNC) &_sweetsoursong_one_flower_ode, 21},
     {"_sweetsoursong_plant_metacomm_stoch_cpp", (DL_FUNC) &_sweetsoursong_plant_metacomm_stoch_cpp, 24},
     {"_sweetsoursong_plant_metacomm_cpp", (DL_FUNC) &_sweetsoursong_plant_metacomm_cpp, 15},
-    {"_sweetsoursong_logit", (DL_FUNC) &_sweetsoursong_logit, 1},
-    {"_sweetsoursong_inv_logit", (DL_FUNC) &_sweetsoursong_inv_logit, 1},
-    {"_sweetsoursong_dissimilarity", (DL_FUNC) &_sweetsoursong_dissimilarity, 2},
-    {"_sweetsoursong_dissimilarity_vector", (DL_FUNC) &_sweetsoursong_dissimilarity_vector, 4},
-    {"_sweetsoursong_diversity", (DL_FUNC) &_sweetsoursong_diversity, 3},
-    {"_sweetsoursong_diversity_vector", (DL_FUNC) &_sweetsoursong_diversity_vector, 4},
     {NULL, NULL, 0}
 };
 
